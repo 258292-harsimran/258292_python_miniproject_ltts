ce4c6f231f040e5645514d76ea6db5174a0c80df
258292-harsimran <harsimranbatra1999@gmail.com>
2021-04-26 14:12:32 +0530

    logic corrected

--- phonebook.py
+++ phonebook.py
@@ -1,11 +1,8 @@
 import sys
   
-# this function will be the first to run as soon as the main function executes
 def initial_phonebook():
     rows, cols = int(input("Please enter initial number of contacts: ")),5
       
-    # We are collecting the initial number of contacts the user wants to have in the
-    # phonebook already. User may also enter 0 if he doesn't wish to enter any.
     phone_book = []
     print(phone_book)
     for i in range(rows):
@@ -14,37 +11,20 @@ def initial_phonebook():
         print("....................................................................")
         temp = []
         for j in range(cols):
-                      
-        # We have taken the conditions for values of j only for the personalized fields
-        # such as name, number, e-mail id, dob, category etc
             if j == 0:
                 temp.append(str(input("Enter name*: ")))
                   
-                # We need to check if the user has left the name empty as its mentioned that
-                # name & number are mandatory fields.
-                # So implement a condition to check as below.
                 if temp[j] == '' or temp[j] == ' ':
                     sys.exit(
                         "Name is a mandatory field. Process exiting due to blank field...")
-                    # This will exit the process if a blank field is encountered.
                       
             if j == 1:
                 temp.append(int(input("Enter number*: ")))
-                # We do not need to check if user has entered the number because int automatically
-                # takes care of it. Int value cannot accept a blank as that counts as a string.
-                # So process automatically exits without us using the sys package.
-                    
         phone_book.append(temp)
-        # By this step we are appending a list temp into a list phone_book
-        # That means phone_book is a 2-D array and temp is a 1-D array
-      
     print(phone_book)
     return phone_book
   
 def menu():
-    # We created this simple menu function for
-    # code reusability & also for an interactive console
-    # Menu func will only execute when called
     print("********************************************************************")
     print("\t\t\tSMARTPHONE DIRECTORY", flush=False)
     print("********************************************************************")
@@ -55,16 +35,12 @@ def menu():
     print("4. Search for a contact")
     print("5. Display all contacts")
     print("6. Exit phonebook")
-  
-    # Out of the provided 6 choices, user needs to enter any 1 choice among the 6
-    # We return the entered choice to the calling function wiz main in our case
     choice = int(input("Please enter your choice: "))
       
     return choice
   
 def add_contact(pb):
-    # Adding a contact is the easiest because all you need to do is:
-    # append another list of details into the already existing list
+    
     dip = []
     for i in range(len(pb[0])):
         if i == 0:
@@ -72,58 +48,35 @@ def add_contact(pb):
         if i == 1:
             dip.append(int(input("Enter number: ")))
     pb.append(dip)
-    # And once you modify the list, you return it to the calling function wiz main, here.
     return pb
   
 def remove_existing(pb):
-    # This function is to remove a contact's details from existing phonebook
     query = str(
-        input("Please enter the name of the contact you wish to remove: "))
-    # We'll collect name of the contact and search if it exists in our phonebook
-      
-    temp = 0
-    # temp is a checking variable here. We assigned a value 0 to temp.
-      
+        input("Please enter the name of the contact you wish to remove: "))      
+    temp = 0      
     for i in range(len(pb)):
         if query == pb[i][0]:
             temp += 1
-            # Temp will be incremented & it won't be 0 anymore in this function's scope
-              
             print(pb.pop(i))
-            # The pop function removes entry at index i
-              
-            print("This query has now been removed")
-            # printing a confirmation message after removal.
-            # This ensures that removal was successful.
-            # After removal we will return the modified phonebook to the calling function
-            # which is main in our program
-              
+            print("This query has now been removed") 
             return pb
     if temp == 0:
-        # Now if at all any case matches temp shoul've incremented but if otherwise,
-        # temp will remain 0 and that means the query does not exist in this phonebook
         print("Sorry, you have entered an invalid query.\
     Please recheck and try again later.")
           
         return pb
   
 def delete_all(pb):
-    # This function will simply delete all the entries in the phonebook pb
-    # It will return an empty phonebook after clearing
     return pb.clear()
   
 def search_existing(pb):
-    # This function searches for an existing contact and displays the result
     choice = int(input("Enter search criteria\n\n\
     1. Name\n2. Number\n3. Email-id\n4. DOB\n5. Category(Family/Friends/Work/Others)\
-    \nPlease enter: "))
-    # We're doing so just to ensure that the user experiences a customized search result
-      
+    \nPlease enter: "))  
     temp = []
     check = -1
       
     if choice == 1:
-    # This will execute for searches based on contact name
         query = str(
             input("Please enter the name of the contact you wish to search: "))
         for i in range(len(pb)):
@@ -132,45 +85,29 @@ def search_existing(pb):
                 temp.append(pb[i])
                   
     elif choice == 2:
-    # This will execute for searches based on contact number
         query = int(
             input("Please enter the number of the contact you wish to search: "))
         for i in range(len(pb)):
             if query == pb[i][1]:
                 check = i
                 temp.append(pb[i])
-                  
-    
+                   
     else:
-    # If the user enters any other choice then the search will be unsuccessful
         print("Invalid search criteria")
         return -1
-    # returning -1 indicates that the search was unsuccessful
-      
-    # all the searches are stored in temp and all the results will be displayed with
-    # the help of display function
-      
     if check == -1:
         return -1
-        # returning -1 indicates that the query did not exist in the directory
     else:
         display_all(temp)
         return check
-        # we're just returning a index value wiz not -1 to calling function just to notify
-        # that the search worked successfully
-  
-# this function displays all content of phonebook pb
 def display_all(pb):
     if not pb:
-    # if display function is called after deleting all contacts then the len will be 0
-    # And then without this condition it will throw an error
         print("List is empty: []")
     else:
         for i in range(len(pb)):
             print(pb[i])
   
 def thanks():
-# A simple gesture of courtesy towards the user to enhance user experience
     print("********************************************************************")
     print("Thank you for using our Smartphone directory system.")
     print("Please visit again!")
@@ -182,9 +119,7 @@ print("....................................................................")
 print("Hello dear user, welcome to our smartphone directory system")
 print("You may now proceed to explore this directory")
 print("....................................................................")
-# This is solely meant for decoration purpose only.
-# You're free to modify your interface as per your will to make it look interactive
-  
+
 ch = 1
 pb = initial_phonebook()
 while ch in (1, 2, 3, 4, 5):
