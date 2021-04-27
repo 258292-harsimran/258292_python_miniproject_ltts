881fc13c681b57d36a2bd6c8e75dbbb6b090b60c
258292-harsimran <harsimranbatra1999@gmail.com>
2021-04-28 00:33:43 +0530

    added file handling

--- phonebook.py
+++ phonebook.py
@@ -1,32 +1,69 @@
 import sys
+
+def contact_storing(temp):
+   file = open("./contact.txt","a")
+   file.write(temp)
+   file.close
+
+def contact_deleting():
+   file = open("./contact.txt","w")
+   file.close
+
+def contact_printing():
+   file = open("./contact.txt","r")
+   print(file.read())
+   file.close
+
+def check_if_string_in_file(string_to_search):
+    #""" Check if any line in the file contains given string """
+    # Open the file in read only mode
+    with open("./contact.txt", 'r') as read_obj:
+        # Read all lines in the file one by one
+        for line in read_obj:
+            # For each line, check if line contains the string
+            if string_to_search in line:
+                return True
+    return False
+
+def test (string1):
+    file1 = open("./contact.txt", "r")
+    
+    # setting flag and index to 0
+    flag = 0
+    index = 0
+    
+    # Loop through the file line by line
+    for line in file1:  
+        index+=1 
+        
+        # checking string is present in line or not
+        if string1 in line:
+            flag=1
+            break 
+            
+    # checking condition for string found or not
+    if flag == 0: 
+        return -1 
+    else: 
+        return index
+    
+    # closing text file    
+    file1.close() 
   
-def initial_phonebook():
-    rows, cols = int(input("Please enter initial number of contacts: ")),5
-      
-    phone_book = []
-    print(phone_book)
-    for i in range(rows):
-        print("\nEnter contact %d details in the following order (ONLY):" % (i+1))
-        print("NOTE: * indicates mandatory fields")
-        print("....................................................................")
-        temp = []
-        for j in range(cols):
-            if j == 0:
-                temp.append(str(input("Enter name*: ")))
-                  
-                if temp[j] == '' or temp[j] == ' ':
-                    sys.exit(
-                        "Name is a mandatory field. Process exiting due to blank field...")
-                      
-            if j == 1:
-                temp.append(int(input("Enter number*: ")))
-        phone_book.append(temp)
-    print(phone_book)
-    return phone_book
+def initial_phonebook():      
+
+    print("\nEnter contact details in the following order" )
+    print("NOTE: * indicates mandatory fields")
+    print("....................................................................")
+    temp_name=input("Enter name*: ")
+    contact_storing("Enter name*: "+temp_name+"\n")
+    temp_number=input("Enter number*: ")
+    contact_storing("Enter number*: "+temp_number+"\n\n")
+    
   
 def menu():
     print("********************************************************************")
-    print("\t\t\tSMARTPHONE DIRECTORY", flush=False)
+    print("\t\t\tPHONEBOOK DIRECTORY")
     print("********************************************************************")
     print("\tYou can now perform the following operations on this phonebook\n")
     print("1. Add a new contact")
@@ -36,105 +73,75 @@ def menu():
     print("5. Display all contacts")
     print("6. Exit phonebook")
     choice = int(input("Please enter your choice: "))
-      
     return choice
+   
+def delete_all():
+    print("\t\tdeleting Contacts\n")
+    contact_deleting()
+    print("All contacts deleted")
+
   
-def add_contact(pb):
-    
-    dip = []
-    for i in range(len(pb[0])):
-        if i == 0:
-            dip.append(str(input("Enter name: ")))
-        if i == 1:
-            dip.append(int(input("Enter number: ")))
-    pb.append(dip)
-    return pb
-  
-def remove_existing(pb):
-    query = str(
-        input("Please enter the name of the contact you wish to remove: "))      
-    temp = 0      
-    for i in range(len(pb)):
-        if query == pb[i][0]:
-            temp += 1
-            print(pb.pop(i))
-            print("This query has now been removed") 
-            return pb
-    if temp == 0:
-        print("Sorry, you have entered an invalid query.\
-    Please recheck and try again later.")
-          
-        return pb
-  
-def delete_all(pb):
-    return pb.clear()
-  
-def search_existing(pb):
-    choice = int(input("Enter search criteria\n\n\
-    1. Name\n2. Number\n3. Email-id\n4. DOB\n5. Category(Family/Friends/Work/Others)\
-    \nPlease enter: "))  
-    temp = []
-    check = -1
+def search_existing():
+    choice_t=int(input("enter your choice \n 1 for searching by name \n 2 for searching by number\n"))
+    if(choice_t==1):
+        print("Please enter the name")
+        s=input()
+        index=test("Enter name*: "+s)
+        if(index==-1):
+            print("Name not present")
+        else:
+            file = open("./contact.txt")
+            # read the content of the file opened
+            content = file.readlines()
+            print(content[index-1])
+            print(content[index])
+    elif(choice_t==2):
+        print("Please enter the number")
+        s=input()
+        index=test("Enter number you want to search: "+s)
+        if(index==-1):
+            print("Number you are searching for is not present in phonebook")
+        else:
+            file = open("./contact.txt")
+            # read the content of the file opened
+            content = file.readlines()
+            print(content[index-2])
+            print(content[index-1])
+    else:
+        print("Invalid choice")
       
-    if choice == 1:
-        query = str(
-            input("Please enter the name of the contact you wish to search: "))
-        for i in range(len(pb)):
-            if query == pb[i][0]:
-                check = i
-                temp.append(pb[i])
-                  
-    elif choice == 2:
-        query = int(
-            input("Please enter the number of the contact you wish to search: "))
-        for i in range(len(pb)):
-            if query == pb[i][1]:
-                check = i
-                temp.append(pb[i])
-                   
-    else:
-        print("Invalid search criteria")
-        return -1
-    if check == -1:
-        return -1
-    else:
-        display_all(temp)
-        return check
-def display_all(pb):
-    if not pb:
-        print("List is empty: []")
-    else:
-        for i in range(len(pb)):
-            print(pb[i])
-  
+
+def display_all():
+    print("\t\tContact List\n")
+    contact_printing()
+
 def thanks():
     print("********************************************************************")
-    print("Thank you for using our Smartphone directory system.")
+    print("Thank you for using our phonebook directory system.")
     print("Please visit again!")
     print("********************************************************************")
     sys.exit("Goodbye, have a nice day ahead!")
   
 # Main function code
 print("....................................................................")
-print("Hello dear user, welcome to our smartphone directory system")
+print("Hello dear user, welcome to our phonebook directory system")
 print("You may now proceed to explore this directory")
 print("....................................................................")
-
-ch = 1
-pb = initial_phonebook()
-while ch in (1, 2, 3, 4, 5):
+ch = 0
+while (True):
     ch = menu()
     if ch == 1:
-        pb = add_contact(pb)
+        initial_phonebook()
     elif ch == 2:
-        pb = remove_existing(pb)
+        pass
     elif ch == 3:
-        pb = delete_all(pb)
+        delete_all()
     elif ch == 4:
-        d = search_existing(pb)
-        if d == -1:
-            print("The contact does not exist. Please try again")
+       search_existing()
     elif ch == 5:
-        display_all(pb)
+        display_all()
+    elif ch == 6:
+        break
     else:
-        thanks()
+        print("Invalid Choice")
+thanks()
